54feb3642e9510e37afadaba9af3c8f8731227c0
Yury Daszukiewicz <ydaszukiewicz@gmail.com>
2023-06-26 15:38:47 +0300

    replaced the app state management with Redux  Toolkit

--- src/features/Login/auth-reducer.ts
+++ src/features/Login/auth-reducer.ts
@@ -1,64 +1,50 @@
-import {Dispatch} from 'redux'
-import {SetAppErrorActionType, setAppStatusAC, SetAppStatusActionType} from '../../app/app-reducer'
-import {authAPI, LoginParamsType} from '../../api/todolists-api'
-import {handleServerAppError, handleServerNetworkError} from '../../utils/error-utils'
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+import { authAPI, LoginParamsType } from "../../api/todolists-api";
+import { appActions } from "../../app/app-reducer";
+import { AppDispatch } from "../../app/store";
+import { handleServerAppError, handleServerNetworkError } from "../../utils/error-utils";
 
-const initialState: InitialStateType = {
-    isLoggedIn: false
-}
-
-export const authReducer = (state: InitialStateType = initialState, action: ActionsType): InitialStateType => {
-    switch (action.type) {
-        case 'login/SET-IS-LOGGED-IN':
-            return {...state, isLoggedIn: action.value}
-        default:
-            return state
-    }
-}
-
-// actions
-
-export const setIsLoggedInAC = (value: boolean) =>
-    ({type: 'login/SET-IS-LOGGED-IN', value} as const)
-
-
+const slice = createSlice({
+  name: "auth",
+  initialState: { isLoggedIn: false },
+  reducers: {
+    setIsLoggedIn: (state, action: PayloadAction<{ isLogged: boolean }>) => {
+      state.isLoggedIn = action.payload.isLogged;
+    },
+  },
+});
+export const authReducer = slice.reducer;
+export const authActions = slice.actions;
 // thunks
-export const loginTC = (data: LoginParamsType) => (dispatch: Dispatch<ActionsType | SetAppStatusActionType | SetAppErrorActionType>) => {
-    dispatch(setAppStatusAC('loading'))
-    authAPI.login(data)
-        .then(res => {
-            if (res.data.resultCode === 0) {
-                dispatch(setIsLoggedInAC(true))
-                dispatch(setAppStatusAC('succeeded'))
-            } else {
-                handleServerAppError(res.data, dispatch)
-            }
-        })
-        .catch((error) => {
-            handleServerNetworkError(error, dispatch)
-        })
-}
-export const logoutTC = () => (dispatch: Dispatch<ActionsType | SetAppStatusActionType | SetAppErrorActionType>) => {
-    dispatch(setAppStatusAC('loading'))
-    authAPI.logout()
-        .then(res => {
-            if (res.data.resultCode === 0) {
-                dispatch(setIsLoggedInAC(false))
-                dispatch(setAppStatusAC('succeeded'))
-            } else {
-                handleServerAppError(res.data, dispatch)
-            }
-        })
-        .catch((error) => {
-            handleServerNetworkError(error, dispatch)
-        })
-}
-
-// types
-
-type ActionsType = ReturnType<typeof setIsLoggedInAC>
-type InitialStateType = {
-    isLoggedIn: boolean
-}
-
-type ThunkDispatch = Dispatch<ActionsType | SetAppStatusActionType | SetAppErrorActionType>
+export const loginTC = (data: LoginParamsType) => (dispatch: AppDispatch) => {
+  dispatch(appActions.setAppStatus({ status: "loading" }));
+  authAPI
+    .login(data)
+    .then((res) => {
+      if (res.data.resultCode === 0) {
+        dispatch(authActions.setIsLoggedIn({ isLogged: true }));
+        dispatch(appActions.setAppStatus({ status: "succeeded" }));
+      } else {
+        handleServerAppError(res.data, dispatch);
+      }
+    })
+    .catch((error) => {
+      handleServerNetworkError(error, dispatch);
+    });
+};
+export const logoutTC = () => (dispatch: AppDispatch) => {
+  dispatch(appActions.setAppStatus({ status: "loading" }));
+  authAPI
+    .logout()
+    .then((res) => {
+      if (res.data.resultCode === 0) {
+        dispatch(authActions.setIsLoggedIn({ isLogged: false }));
+        dispatch(appActions.setAppStatus({ status: "succeeded" }));
+      } else {
+        handleServerAppError(res.data, dispatch);
+      }
+    })
+    .catch((error) => {
+      handleServerNetworkError(error, dispatch);
+    });
+};
